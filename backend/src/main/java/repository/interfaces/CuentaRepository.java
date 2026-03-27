de249aa19672013aee98389b264200598139f46e
Joan <drdoffensmirhtz@gmail.com>
2026-03-27 16:16:18 +0000

    possible final version of interfaces for models

--- backend/src/main/java/repository/interfaces/CuentaRepository.java
+++ backend/src/main/java/repository/interfaces/CuentaRepository.java
@@ -1,6 +1,13 @@
 package repository.interfaces;
 
 import model.Cuenta;
+import model.Mesa;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CuentaRepository extends Repository<Cuenta, String> {
+    Optional<Cuenta> findByMesa(Mesa mesa);
+    List<Cuenta> findByPayed(Boolean payed);
 }
+
--- backend/src/main/java/repository/interfaces/PlatoRepository.java
+++ backend/src/main/java/repository/interfaces/PlatoRepository.java
@@ -1,6 +1,12 @@
 package repository.interfaces;
 
+import model.Categoria;
 import model.Plato;
 
+import java.util.List;
+
 public interface PlatoRepository extends Repository<Plato, String> {
+    List<Plato> findByCategoria(Categoria categoria);
+    List<Plato> findByActivo(Boolean activo);
+    List<Plato> findByNombre(String nombre);
 }
