e4612de0ea833d093101f1397a09014663ecaef8
Alejandro Hernández <alejandro.hdelgado144@gmail.com>
2026-04-30 02:18:33 +0100

    - Añadido login y mesas de camarero

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { BillPage } from './pages/cliente/bill-page/bill-page';
 import { TableroPedidos } from './pages/cocina/tablero-pedidos';
 import { MenuCamarero } from './pages/camarero/menu/menu';
 import { PedidoCamarero } from './pages/camarero/pedido/pedido';
+import {LoginCamarero} from './pages/camarero/login/login';
+import {MesasCamarero} from './pages/camarero/mesas/mesas';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'mesas', pathMatch: 'full' },
@@ -25,4 +27,6 @@ export const routes: Routes = [
   { path: 'cocina', component: TableroPedidos },
   { path: 'camarero/menu/:id', component: MenuCamarero },
   { path: 'camarero/pedido/:id', component: PedidoCamarero },
+  { path: 'camarero/login', component: LoginCamarero },
+  { path: 'camarero/mesas', component: MesasCamarero },
 ];
